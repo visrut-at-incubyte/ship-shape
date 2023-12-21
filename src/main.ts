165a5c313ed5f04f9131f1e539b30f5c244510d2
visrut <visrut@incubyte.co>
2023-12-21 22:14:06 +0530

    bring figure to at the center

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ class DrawingCanvas {
   private state: {
     drawing: boolean;
     lastPoint: Point;
+    points: Point[];
   };
 
   constructor(canvasId: string) {
@@ -18,6 +19,7 @@ class DrawingCanvas {
     this.state = {
       drawing: false,
       lastPoint: { x: 0, y: 0 },
+      points: [],
     };
   }
 
@@ -27,6 +29,29 @@ class DrawingCanvas {
 
     this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
     this.ctx.scale(1, -1); // Flip the y-axis
+
+    this.drawAxis();
+  }
+
+  private drawAxis() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, window.innerHeight / 2);
+    this.ctx.lineTo(0, -window.innerHeight / 2);
+    this.ctx.moveTo(-window.innerWidth / 2, 0);
+    this.ctx.lineTo(window.innerWidth / 2, 0);
+    this.ctx.strokeStyle = "white";
+    this.ctx.lineWidth = 1;
+    this.ctx.stroke();
+  }
+
+  private resetCanvas() {
+    this.ctx.clearRect(
+      -this.canvas.width / 2,
+      -this.canvas.height / 2,
+      this.canvas.width,
+      this.canvas.height
+    );
+    this.drawAxis();
   }
 
   private addEventListeners() {
@@ -50,7 +75,6 @@ class DrawingCanvas {
     if (!this.state.drawing) return;
 
     const newPoint = this.convertToCartesianCoords(event);
-    this.drawLine(this.state.lastPoint, newPoint);
     this.drawArc(newPoint);
 
     this.state.lastPoint = newPoint;
@@ -60,18 +84,11 @@ class DrawingCanvas {
     this.state.drawing = false;
   }
 
-  private drawLine(from: Point, to: Point) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(from.x, from.y);
-    this.ctx.lineTo(to.x, to.y);
-    this.ctx.strokeStyle = "white";
-    this.ctx.lineWidth = 10;
-    this.ctx.stroke();
-  }
-
   private drawArc(point: Point) {
     this.ctx.beginPath();
+    this.ctx.lineWidth = 10;
     this.ctx.arc(point.x, point.y, this.ctx.lineWidth / 2, 0, Math.PI * 2);
+    this.state.points.push(point);
     this.ctx.fillStyle = "white";
     this.ctx.fill();
   }
@@ -84,6 +101,34 @@ class DrawingCanvas {
 
     return { x, y };
   }
+
+  public play() {
+    const points = this.state.points.filter(
+      (point) => !isNaN(point.x) && !isNaN(point.y)
+    );
+
+    const centroid = this.getCentroid(points);
+    this.drawArc(centroid);
+
+    for (const point of points) {
+      point.x -= centroid.x;
+      point.y -= centroid.y;
+    }
+
+    for (const point of points) {
+      this.drawArc(point);
+    }
+  }
+
+  private getCentroid(points: Point[]): Point {
+    const x = points.reduce((sum, point) => sum + point.x, 0) / points.length;
+    const y = points.reduce((sum, point) => sum + point.y, 0) / points.length;
+    return { x, y };
+  }
 }
 
 export const drawingCanvas = new DrawingCanvas("canvas");
+const playButton = document.getElementById("play-button") as HTMLButtonElement;
+playButton.addEventListener("click", () => {
+  drawingCanvas.play();
+});
