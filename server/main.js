29cce540ac41d0ccedab793e0b32e0c2c781178e
visrut <visrut@incubyte.co>
2023-12-23 14:52:09 +0530

    feat: add server for saving the data

---
+++ server/main.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(cors());
+
+const cacheDir = "./.cache";
+
+if (!fs.existsSync(cacheDir)) {
+  fs.mkdirSync(cacheDir, { recursive: true });
+}
+
+app.post("/save", (req, res) => {
+  const data = req.body;
+  const typeDir = path.join(cacheDir, data.type);
+
+  fs.mkdirSync(typeDir, { recursive: true });
+
+  fs.readdir(typeDir, (err, files) => {
+    if (err) {
+      console.error("Error reading directory", err);
+      res.status(500).send("Error reading directory");
+      return;
+    }
+
+    const relevantFiles = files
+      .filter((file) => file.endsWith(`${data.type}.json`))
+      .map((file) => parseInt(file))
+      .sort((a, b) => b - a);
+
+    const lastNumber = relevantFiles.length > 0 ? relevantFiles[0] : 0;
+    const nextNumber = lastNumber + 1;
+    const paddedNumber = String(nextNumber).padStart(4, "0");
+    const filename = path.join(typeDir, `${paddedNumber}${data.type}.json`);
+
+    fs.writeFile(filename, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error("Error saving data", err);
+        res.status(500).send("Error saving data");
+        return;
+      }
+      res.send("Data saved successfully");
+    });
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
